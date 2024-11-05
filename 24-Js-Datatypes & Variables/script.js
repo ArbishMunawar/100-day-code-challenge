e59e4cf25630bee42624b90bf90c04fa590620e4
ArbishMunawar <munawararbish159@gmail.com>
2024-11-05 08:56:44 -0800

    Day-25/Js-Datatypes & Variables

---
+++ 24-Js-Datatypes & Variables/script.js
@@ -0,0 +1,42 @@
+// Values
+
+// String
+console.log("Sring========","Js in fun");
+
+// Number
+console.log("Number========",100);
+
+// Booleans
+console.log("Boolean true========",true);
+console.log("Boolean false========",false);
+
+// Null
+console.log("Null========",null);
+
+// Undefined
+console.log("Undefined========",undefined);
+
+// Datatypes in js - 7
+// 1- String
+// 2- Number
+// 3- Boolean
+// 4- Null
+// 5- Undefined
+// 6- Symbol
+// 7- BigInt
+
+
+// Variables
+age = 88;
+console.log("Type of age is=====",typeof age);
+
+age = "100";
+console.log("Type of age is=====",typeof age);
+
+firstname = 'Arbish'
+// lastname = Munawar
+console.log("Type of Firts name is=====",typeof firstname);
+console.log("Type of true is=====",typeof true);
+
+console.log("Type of undefined is=====",typeof undefined);
+console.log("Type of null is=====",typeof null);
